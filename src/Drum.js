f42149b290df68323c2fd75a138891b71293bfd1
Tyler Oliver <65270924+TylerOliver197@users.noreply.github.com>
2021-11-04 20:35:17 -0400

    initial commit

--- src/Drum.js
+++ src/Drum.js
@@ -1,25 +1,16 @@
-import React from "react";
-import bankOne from "./Object";
-import Key from "./Key";
+import React, { Component } from "react";
 
-export default function Drum(props) {
-  return (
-    <div id="drum">
-      <section className="drum-row">
-        <Key letter={bankOne[0]} onClick={props.onClick} />
-        <Key letter="W" onClick={props.onClick} />
-        <Key letter="E" onClick={props.onClick} />
-      </section>
-      <section className="drum-row">
-        <Key letter="A" onClick={props.onClick} />
-        <Key letter="S" onClick={props.onClick} />
-        <Key letter="D" onClick={props.onClick} />
-      </section>
-      <section className="drum-row">
-        <Key letter="Z" onClick={props.onClick} />
-        <Key letter="X" onClick={props.onClick} />
-        <Key letter="C" onClick={props.onClick} />
-      </section>
-    </div>
-  );
+export default class Drum extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div></div>;
+  }
+  //this.handleChange=this.handleChange.bind(this)
+
+  //add event listeners
+
+  //render
+  //return
 }
---
+++ src/DrumPad.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Drum from "./Drum";
+
+export default class Key extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Drum />;
+  }
+}
